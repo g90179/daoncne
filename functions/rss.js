6d7f2de9776b8b200fd376bb1e794242a663c47f
g901 <dev.g901@ofu.co.kr>
2026-07-13 14:41:08 +0900

    포털 검색 (노출) 작업 (14) : RSS 등록 => rss.js 테스트

--- functions/rss.js
+++ functions/rss.js
@@ -1,15 +1,19 @@
 export async function onRequest(context) {
-  const targetUrl = "https://g90179.gabia.io/rss";
+//   const targetUrl = "https://g90179.gabia.io/rss";
   
-  // 기존 브라우저 헤더를 복사하지 않고, 가비아가 요구하는 핵심 헤더만 깔끔하게 생성
-  const cleanRequest = new Request(targetUrl, {
-    method: "GET",
-    headers: {
-      "Host": "g90179.gabia.io", // 나는 가비아 도메인이다 (변조)
-      "Accept": "application/rss+xml, application/xml, text/xml, */*"
-    }
+//   // 기존 브라우저 헤더를 복사하지 않고, 가비아가 요구하는 핵심 헤더만 깔끔하게 생성
+//   const cleanRequest = new Request(targetUrl, {
+//     method: "GET",
+//     headers: {
+//       "Host": "g90179.gabia.io", // 나는 가비아 도메인이다 (변조)
+//       "Accept": "application/rss+xml, application/xml, text/xml, */*"
+//     }
+//   });
+  
+//   // 백엔드로 요청 전송
+//   return fetch(cleanRequest);
+
+  return new Response("functions/rss.js 가 정상 작동 중입니다!", {
+    headers: { "content-type": "text/html; charset=utf-8" },
   });
-  
-  // 백엔드로 요청 전송
-  return fetch(cleanRequest);
 }
